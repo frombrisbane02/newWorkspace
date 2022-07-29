7634a769075d90211bea3655551e8f20b97e2499
shindongmi <106238120+shindongmi@users.noreply.github.com>
2022-07-29 13:35:19 +0900

    Create UsersDAO.java

---
+++ PictoryRestService/src/main/java/com/pictory/rest/service/impl/UsersDAO.java
@@ -0,0 +1,34 @@
+package com.pictory.rest.service.impl;
+
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.pictory.rest.service.UsersDTO;
+
+@Repository
+public class UsersDAO {
+
+	@Autowired
+	private SqlSessionTemplate template;
+	
+	public boolean isJoin(Map map) {
+		return (Integer)template.selectOne("UsersIsJoin",map)==1?true:false;
+	}
+	
+	public UsersDTO selectOne(Map map) {
+		return template.selectOne("selectOneUser",map);
+	}
+	
+	public int insertUsers(Map map) {
+		template.insert("usersInsert",map);
+		return template.insert("authoritiesInsert");
+	}
+
+
+
+	
+	
+}
